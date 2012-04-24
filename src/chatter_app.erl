b5f2842452a9f5d5933ba6c0003bbe8c532a807e
Marco Paga <marco.paga@gmail.com>
2012-04-24 20:53:34 +0200

    initial and not working

---
+++ src/chatter_app.erl
@@ -0,0 +1,17 @@
+-module(chatter_app).
+
+-behaviour(application).
+
+%% Application callbacks
+-export([start/2, stop/1]).
+
+%% ===================================================================
+%% Application callbacks
+%% ===================================================================
+
+start(_StartType, _StartArgs) ->
+    db:create_tables(),
+    chatter_sup:start_link().
+
+stop(_State) ->
+    ok.
---
+++ src/db.erl
@@ -0,0 +1,10 @@
+-module(db).
+
+-record(user, {id, email}).
+-record(message, {from_user_id, to_user_id, text, timestamp}).
+
+-export([create_tables/0]).
+
+create_tables() ->
+    mnesia:create_table(user, [{attributes, record_info(fields, user)}]),
+    mnesia:create_table(message, [{attributes, record_info(fields, message)}]).
