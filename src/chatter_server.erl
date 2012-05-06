1e13925e0075d4cb08e508f30f9a3c8264814f87
Marco Paga <marco.paga@gmail.com>
2012-05-06 19:27:23 +0200

    just a bit hacking on the socket handling

--- src/chatter_server.erl
+++ src/chatter_server.erl
@@ -3,7 +3,7 @@
 -define(SERVER, ?MODULE).
 -define(LISTEN_PORT, 9090).
 
--record(state, {listening_Socket, userEmail}).
+-record(state, {listening_socket, userEmail}).
 %% ------------------------------------------------------------------
 %% API Function Exports
 %% ------------------------------------------------------------------
@@ -29,9 +29,9 @@ start_link() ->
 %% ------------------------------------------------------------------
 
 init(_Args) ->
-	{ok, Socket} = gen_tcp:listen(?LISTEN_PORT, [{active, true}]),
-	TimeoutToContinueInHandleInfo = 0, 
-    {ok, #state{listening_Socket = Socket}, TimeoutToContinueInHandleInfo}.
+	{ok, ListeningSocket} = gen_tcp:listen(?LISTEN_PORT, [{active, true}]),
+	TimeoutToContinueInitializationInHandleInfo = 0, 
+    {ok, #state{listening_socket = ListeningSocket, userEmail = ""}, TimeoutToContinueInitializationInHandleInfo}.
 
 handle_call(_Request, _From, State) ->
     {reply, ok, State}.
@@ -39,18 +39,26 @@ handle_call(_Request, _From, State) ->
 handle_cast(_Msg, State) ->
     {noreply, State}.
 
-handle_info({tcp,_Socket,RawData}, _State) ->
-	error_logger:info_msg("Data received: ~s \n", [RawData]),
-	{noreply, _State};
-handle_info(timeout, #state{listening_Socket = Listening_Socket} = State) ->
-	error_logger:info_msg("Accepting socket connection. \n"),
-	{ok, _Sock} = gen_tcp:accept(Listening_Socket),
+handle_info({tcp,Socket,RawData}, State = #state{} ) when State#state.userEmail == "" ->
+	{success, UserEmail} = try_authentication(Socket, RawData),
+	{noreply, State#state{userEmail = UserEmail}};
+
+handle_info({tcp,Socket,RawData}, State = #state{} ) ->
+	error_logger:info_msg("~s : ~s",[State#state.userEmail, RawData]),
+	gen_tcp:send(Socket,RawData),
 	{noreply, State};
+
+handle_info(timeout, #state{listening_socket = ListeningSocket} = State) ->
+	error_logger:info_msg("Accepting socket connections. \n"),
+	{ok, Socket} = gen_tcp:accept(ListeningSocket),
+	gen_tcp:send(Socket, "Please authenticate. Type your username/password\n"),
+	{noreply, State};
+
 handle_info(_Info, State) ->
     {noreply, State}.
 
-terminate(_Reason, #state{listening_Socket = Listening_Socket} = _State) ->
-	gen_tcp:close(Listening_Socket),
+terminate(_Reason, #state{listening_socket = ListeningSocket} = _State) ->
+	gen_tcp:close(ListeningSocket),
     ok.
 
 code_change(_OldVsn, State, _Extra) ->
@@ -59,4 +67,6 @@ code_change(_OldVsn, State, _Extra) ->
 %% ------------------------------------------------------------------
 %% Internal Function Definitions
 %% ------------------------------------------------------------------
-
+try_authentication(Socket, RawData) ->
+	gen_tcp:send(Socket, "\nUsing your input as username!"),
+	{success, RawData}.
